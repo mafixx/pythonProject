e8f042e51e57af025aa80e5299c93e558d5a9e6c
Pedro Barbieri <pedro.romaguera@hotmail.com>
2022-11-16 20:26:59 -0300

    Aula 01

---
+++ 002.py
@@ -0,0 +1,10 @@
+"""
+Entrada e saída padrão em Python
+
+Utilizamos 2 funções:
+    print() -> Imprime um texto na tela
+    input() -> Recebe uma informação pelo teclado
+"""
+
+if __name__ == "__main__":
+    pass
