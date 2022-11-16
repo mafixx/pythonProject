b0f6917edcc2f8447c95c5dcd2f0925591079195
Pedro Barbieri <pedro.romaguera@hotmail.com>
2022-11-16 20:22:01 -0300

    Aula 01

---
+++ 001.py
@@ -0,0 +1,35 @@
+# Esse é um comentário
+
+"""
+Isso é uma string multilinha
+É utilizada para gerar documentação de funções, métodos e classes no Python
+Também pode ser utilizada como comentário
+"""
+
+"""
+Executamos um arquivo python utilizando a seguinte sintaxe:
+python <nome_do_arquivo.py>
+"""
+
+# Imprime a mensagem "Hello, world!" no terminal.
+
+
+"""
+
+Não é obrigatório, mas é recomendável utilizarmos a condição 'if __name__ == '__main__': 
+no arquivo principal da aplicação, indicando que ele é o entrypoint da aplicação.
+
+Os ":" indicam o ínicio de um novo bloco de código.
+
+ATENÇÃO!!!
+*** Todo bloco de código deve ter o espaçamento de código correto em relação ao anterior ***
+
+O ";" somente é utilizado quando se quer ter mais de um statement (comando) por linha, contudo, não é recomendado
+o seu uso.
+
+É muito importante seguir a identação correta dos blocos, a fim de evitar exceções do tipo IdentationError.
+
+"""
+
+if __name__ == "__main__":
+    print("Hello, world!")
