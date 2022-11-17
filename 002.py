a2633af6ccdbeff0f00d58d4219d246073317ce5
Pedro Barbieri <pedro.romaguera@hotmail.com>
2022-11-16 21:18:02 -0300

    aula

--- 002.py
+++ 002.py
@@ -4,7 +4,29 @@ Entrada e saída padrão em Python
 Utilizamos 2 funções:
     print() -> Imprime um texto na tela
     input() -> Recebe uma informação pelo teclado
+
+Conversor de reais para dólares
+
+A função input sempre retorna uma string, independentemente do valor informado no terminal.
+
+Para operações matemáticas, é necessário converter o valor de entrada (String) em um valor numérico.
+
+Nesse caso, utilizamos as funções built-in int() e/ou float()
+
+O "f" no início da string significa f-string. Utilizando f-strings, conseguimos inserir facilmente expressões python
+dentro de string.
+
+O .format() ao final, depois das aspas,tem a mesma função do "f-string". Ficando dessa forma:
+print(f"O valor convertido é igual a: ".format(valor_convertido))
+
+O :.2f limita o valor que aparece em 2 casas após a vírgula nos casos de float.
+
 """
 
 if __name__ == "__main__":
-    pass
+    valor_em_reais = float(input("Insira o valor em reais: "))
+    cotacao_do_dolar = float(input("Insira a cotação atual do dólar: "))
+
+    valor_convertido = valor_em_reais * cotacao_do_dolar
+
+    print(f"O valor convertido é igual a: {valor_convertido:.2f}")
